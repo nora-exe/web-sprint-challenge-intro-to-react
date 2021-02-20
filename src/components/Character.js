1ad4a2752f93bc3e887611afa1fb6795a22bace2
Nora C <nocorser@gmail.com>
2021-02-19 18:43:05 -0600

    styling works

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,22 @@
 //🌟 Character component
-// Import React
+// Import React, styles
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledCharacter = styled.div`
+font-weight: bold;
+color: orange;
+`
 
 const Character = props => {
     const {character} = props;
     return (
+        <StyledCharacter>
         <div>
             <p className="characterName">{character.name}</p>
             <p className="characterBorn">{character.birth_year}</p>
         </div>
+        </StyledCharacter>
     )
 }
 export default Character;
