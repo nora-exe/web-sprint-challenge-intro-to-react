d46189433332924501217c206ab456df21ccc663
Nora C <nocorser@gmail.com>
2021-02-19 18:34:27 -0600

    character API calls functional

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
-// Write your Character component here
+//🌟 Character component
+// Import React
+import React from 'react';
+
+const Character = props => {
+    const {character} = props;
+    return (
+        <div>
+            <p className="characterName">{character.name}</p>
+            <p className="characterBorn">{character.birth_year}</p>
+        </div>
+    )
+}
+export default Character;
+
+// TODO data I eventually want to select for in App.js helper f: {name}{species}{homeworld}
+
+//TODO styling
